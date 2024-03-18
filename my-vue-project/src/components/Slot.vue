e2878de4f5d3afd981e8b394c33485d7b2e196fb
Lipsa Kevadiya <lipsa.kevadiya@exhibytesolution.com>
2024-03-18 18:43:58 +0530

    Installed and setup my-vue-project, click events, methods, watchers, slots , computed() property , interpolation and data() property.

---
+++ my-vue-project/src/components/Slot.vue
@@ -0,0 +1,35 @@
+<template>
+<h1>Slots in vue js 3</h1>
+
+<ChildCompo>
+<template v-slot:header><h1 >Lipsa</h1></template>
+<template v-slot:main><button>Click Me</button></template>
+<template v-slot:footer><p>Mutiple slots with name</p></template>
+</ChildCompo>
+
+<ChildCompo>
+<template v-slot:header><h1 >Meet</h1></template>
+<template v-slot:main><button>Click Me</button></template>
+<template v-slot:footer><p>Mutiple slots with name</p></template>
+</ChildCompo>
+<!-- <ChildCompo><h1>Html h1 tag</h1></ChildCompo>
+<ChildCompo><a href="#">Link</a></ChildCompo> -->
+</template>
+
+<script>
+import ChildCompo from './Child.vue'
+export default {
+  name: 'SlotCompo',
+  components: {
+    ChildCompo
+  }
+
+
+}
+</script>
+
+<style scoped>
+h1 {
+  color: orange
+}
+</style>
